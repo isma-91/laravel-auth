561c025e492e03a738db6c8e95dea8aeda313334
isma-91 <isma91@hotmail.it>
2023-01-26 14:39:52 +0100

    Populate database with postSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //Se le tabelle dei seeder qui sotto elencate sono collegate tra loro, mettere i seeder nell'ordine corretto così che venga popolata prima la tabella, che per esempio, deve fornire una chiave esterna ad un altra tabella, altrimenti quest'ultima verrebbe creata prima che sia presente la tabella che gli deve passare la chiave esterna e darebbe errore.
+        //Se le tabelle dei seeder qui sotto elencate sono collegate tra loro, mettere queste righe di seeder nell'ordine corretto così che venga popolata prima la tabella, che per esempio, deve fornire una chiave esterna ad un altra tabella, altrimenti quest'ultima verrebbe creata prima che sia presente la tabella che gli deve passare la chiave esterna e darebbe errore.
         $this->call(UserSeeder::class);
-        // $this->call(PostSeeder::class);
+        $this->call(PostSeeder::class);
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -32,6 +32,7 @@ class UserSeeder extends Seeder
             ],
         ];
 
+        // Qui possiamo usare questo metodo invece di elencare tutte le voci e senza andare a "dare i permessi per il fill" dal model perchè siamo nei seed, quindi ci mettiamo le mani noi developer e dovremmo sapere quello che stiamo facendo e quindi che tipo di dato stiamo inserendo senza inguaiare il DB, mentre nel controller meglio usare la notazione lunga campo per campo, perchè li vengono passati i dati che ci da l'utente e quindi meglio controllare precisamente cosa ci sta passando, altrimenti ci potrebbe passare dati che non vanno bene/che non rispettano le regole stabilite dal DB e inguaiare tutto.
         foreach ($users as $user) {
             User::create($user);
         }
