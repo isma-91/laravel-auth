a4a97a6f6dce862e85a4d6aa087fd2aec8fcea9c
isma-91 <isma91@hotmail.it>
2023-01-26 14:27:38 +0100

    populate user table with user seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //Se le tabelle dei seeder qui sotto elencate sono collegate tra loro, mettere i seeder nell'ordine corretto così che venga popolata prima la tabella, che per esempio, deve fornire una chiave esterna ad un altra tabella, altrimenti quest'ultima verrebbe creata prima che sia presente la tabella che gli deve passare la chiave esterna e darebbe errore.
         $this->call(UserSeeder::class);
-        $this->call(PostSeeder::class);
+        // $this->call(PostSeeder::class);
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,27 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users =
+        [
+            [
+                'name'      => 'asdf',
+                'email'     => 'asdf@asdf.asdf',
+                'password'  => Hash::make('asdf'),
+            ],
+            [
+                'name'      => 'qwert',
+                'email'     => 'qwert@qwert.qwert',
+                'password'  => Hash::make('qwert'),
+            ],
+            [
+                'name'      => 'zxcv',
+                'email'     => 'zxcv@zxcv.zxcv',
+                'password'  => Hash::make('zxcv'),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
